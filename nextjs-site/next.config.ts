4cb98a71c9a5b375b8e5d6553994f46cb0e34562
ddddasdfs <joao3almeida@protonmail.com>
2026-01-28 05:55:48 -0300

    Remove letter generator page - images now hosted on Netlify

--- nextjs-site/next.config.ts
+++ nextjs-site/next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        // Map /images/a1.png, /images/b1.png, etc. to /api/letter/[letter]
-        source: '/images/:letter(\\w)1.png',
-        destination: '/api/letter/:letter',
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
